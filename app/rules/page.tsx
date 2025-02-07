2b0ecc76f0fc1a8fcc9b7302cec1e3aa8bae4eda
Marco <marco@midachat.com>
2025-02-07 17:41:45 +0100

    rules

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -179,7 +179,7 @@ export default function RulesPage() {
           />
 
           <ThemesAnalysisDialog
-            selectedHotelId={selectedHotelId}
+            hotelId={selectedHotelId}
             isOpen={isThemesDialogOpen}
             onClose={() => setIsThemesDialogOpen(false)}
             onAnalysisStart={() => {
